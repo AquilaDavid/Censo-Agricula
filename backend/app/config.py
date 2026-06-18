a597f4e42c55ff7288cf2a4686accaff98966bf3
AquilaDavid <aquiladavid07@gmail.com>
2026-06-18 14:23:55 -0300

    fix: adicionando configuração de conexão com redis

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{os.getenv('DB_USER')}:"
@@ -15,4 +14,11 @@ class Config:
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     LOG_LEVEL = logging.INFO
+
+    #TODO: Add Redis configuration
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis-cache")
+    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+    REDIS_DB = int(os.getenv("REDIS_DB", 0))
+    CACHE_TTL = 3000
